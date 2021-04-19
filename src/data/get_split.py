637524352b5ae48f9d3adb47fea9211e92217543
Zachary Zipper <zezip@umich.edu>
2021-04-19 16:26:47 -0400

    added code to make and get the split

---
+++ src/data/get_split.py
@@ -0,0 +1,46 @@
+import numpy as np
+from pathlib import Path
+import os.path
+from glob import glob
+
+DIR = Path(os.path.abspath('')).resolve()
+ROOT = DIR.parent.parent
+# SPLIT = str(ROOT/"data"/"split.npz")
+SPLIT = str(ROOT/"data"/"split"/"split.npz")
+
+def get_split(verbose=False):
+    """
+        If this doesn't work (file not found), you might need to run the main of make_split.py (which calls 'make_split') first
+        Loads and returns a tuple of:
+            (X_train, y_train, X_test, y_test), index_to_tag
+        s.t.:
+            *_train is a 1d numpy array of strings (one per sample)
+            *_test is a 2d numpy array of tag indices (len(kept_tags) per sample)
+            index_to_tag is a numpy array mapping indices to their tag string
+    """
+    # with open(SPLIT, 'rb') as f:
+    #     return pickle.load(f)
+    loaded = np.load(SPLIT)
+    X_train = loaded['X_train']
+    X_test = loaded['X_test']
+    y_train = loaded['y_train']
+    y_test = loaded['y_test']
+    index_to_tag = loaded['index_to_tag']
+    return (X_train, y_train, X_test, y_test), index_to_tag
+
+def save_split(X_train, y_train, X_test, y_test, index_to_tag, verbose=False):
+    """
+        Called by 'make_split' to serialize the split to a shared location
+    """
+    # with open(SPLIT, 'wb') as f:
+    #     pickle.dump(data, f)
+    np.savez_compressed(SPLIT, X_train=X_train, X_test=X_test, y_train=y_train, y_test=y_test, index_to_tag=index_to_tag)
+
+
+if __name__ == "__main__":
+    (X_train, y_train, X_test, y_test), index_to_tag = get_split(verbose=True)
+    print('X_train', X_train.shape, type(X_train))
+    print('y_train', y_train.shape, type(y_train))
+    print('X_test', X_test.shape, type(X_test))
+    print('y_test', y_test.shape, type(y_test))
+    print('index_to_tag', len(index_to_tag), type(index_to_tag))
---
+++ src/data/make_split.py
@@ -0,0 +1,63 @@
+from load_and_clean_chunked import load_and_clean_chunked
+from get_split import save_split, SPLIT
+from tqdm import tqdm
+import numpy as np
+import pandas as pd
+from collections import Counter
+from sklearn.utils import shuffle
+# see http://scikit.ml/stratification.html to see how the multilabel stratification works
+# You probably need to run `pip install scikit-multilearn`
+from skmultilearn.model_selection import iterative_train_test_split as train_test_split
+
+SEED = 1
+TEST_SIZE = 0.2
+# To be determined by frequency analysis
+MIN_N = 1000
+
+def make_split(verbose=False):
+    """
+        Do not call this function to get the split, call 'get_split()' in get_split.py
+        Call this function to filter, split, and serialize your data
+    """
+    df = load_and_clean_chunked(verbose=verbose)
+
+    kept_tags = get_kept_tags(df)
+    index_to_tag = np.array(list(kept_tags))
+    tag_to_index = {tag : index for index, tag in enumerate(index_to_tag)}
+
+    X, y = [], []
+
+    # Iterate through df, keep entries that have at least one label in our 'kept_tags' set, squish title and body to make X
+    if verbose:
+        print("Removing infrequent tags...")
+    for _, row in tqdm(df.iterrows(), disable=not verbose, total=len(df)):
+        y_init = np.zeros(len(kept_tags))
+        for tag in row['tags']:
+            try:
+                y_init[tag_to_index[tag]] = 1
+            except KeyError:
+                pass
+        # If sum is 0, no relevant tags are kept
+        if y_init.sum() > 0:
+            y.append(y_init)
+            X.append(row['title'] + ' ' + row['body'])
+    if verbose:
+        print(f"Kept {len(kept_tags)} tags and {len(X)} samples...")
+        print("Shuffling...")
+    X, y = shuffle(np.array(X).reshape(-1, 1), np.array(y), random_state=SEED)
+    if verbose:
+        print("Splitting...")
+    X_train, y_train, X_test, y_test = train_test_split(X, y, TEST_SIZE)
+    save_split(X_train, y_train, X_test, y_test, index_to_tag)
+    print(f"Split has been serialized to {SPLIT}.")
+
+def get_kept_tags(df):
+    tag_to_frequency = Counter()
+    # Counting tag frequencies
+    for tags in df['tags']:
+        for tag in tags:
+            tag_to_frequency[tag] += 1
+    return {tag for tag, frequency in tag_to_frequency.items() if frequency >= MIN_N}
+
+if __name__ == "__main__":
+    make_split(verbose=True)
