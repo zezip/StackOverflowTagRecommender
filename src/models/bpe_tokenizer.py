0bf6f6ae12218ee9b386f40c6b5c9e4f450059b5
Zachary Zipper <zezip@umich.edu>
2021-04-22 19:13:31 -0400

    finished BPETokenizer- it has a matching API to Countvectorizer, and produces a BOW output of the learned vocab

--- src/models/bpe_tokenizer.py
+++ src/models/bpe_tokenizer.py
@@ -7,7 +7,10 @@ from tokenizers import Tokenizer
 from tokenizers.models import BPE
 from tokenizers.trainers import BpeTrainer
 from tokenizers.pre_tokenizers import Whitespace
+from tokenizers.normalizers import Sequence, Lowercase, NFD, StripAccents
+from collections import Counter
 import errno
+import numpy as np
 
 DIR = Path(os.path.abspath('')).resolve()
 SRC = DIR.parent
@@ -18,6 +21,36 @@ sys.path.append(str(SRC/"data"))
 from load_and_clean_chunked import load_and_clean_chunked
 
 VOCAB_SIZE = 30000
+
+def load_bpe_encoder(verbose=False):
+    """
+        Loads the encoder as an object that matches the CountVectorizer API/signature
+    """
+    return BPEVectorizer(Tokenizer.from_file(TOKENIZER))
+
+class BPEVectorizer:
+    def __init__(self, tokenizer):
+        self.tokenizer = tokenizer
+        self.size = tokenizer.get_vocab_size()
+
+    def fit(self, X):
+        new_tokens = self.tokenizer.encode(' '.join(X)).tokens
+        self.tokenizer.add_tokens(new_tokens)
+
+    def transform(self, X):
+        y = self.tokenizer.encode_batch(X)
+        res = np.zeros((len(X), self.size))
+        for i in range(len(y)):
+            for j in y[i].ids:
+                res[i, j] += 1
+        return res
+
+    def fit_transform(self, X):
+        """
+            Return X as a sparse matrix (bag-of-words)
+        """
+        self.fit(X)
+        return self.transform(X)
 
 def try_mkdir(dirname):
     try:
@@ -31,6 +64,7 @@ def train_bpe_encoder(verbose=False):
     tokenizer = Tokenizer(BPE(unk_token="[UNK]"))
     tokenizer.pre_tokenizer = Whitespace()
     trainer = BpeTrainer(special_tokens=["[UNK]"], vocab_size=VOCAB_SIZE)
+    tokenizer.normalizer = Sequence([NFD(), Lowercase(), StripAccents()])
     
     if verbose:
         print('Training encoder...')
@@ -41,11 +75,15 @@ def train_bpe_encoder(verbose=False):
 
     tokenizer.save(TOKENIZER)
     return tokenizer
-
-def load_bpe_encoder(verbose=False):
-    return Tokenizer.from_file(TOKENIZER)
-    
+  
 if __name__ == "__main__":
-    tokenizer = train_bpe_encoder(verbose=True)
-    print(tokenizer.encode("Hello, y'all! How are you?"))
-    print(tokenizer.encode_batch(["Hello, y'all!", "How are you?"]))
+    # tokenizer = train_bpe_encoder(verbose=True)
+    # print(tokenizer.encode("Hello, y'all! How are you?"))
+    # print(tokenizer.encode_batch(["Hello, y'all!", "How are you?"]))
+    tokenizer = load_bpe_encoder()
+    X = [
+        "The quick brown fox OOGOGs",
+        "jumped over the",
+        "lazy dog"
+    ]
+    y = tokenizer.fit_transform(X)
